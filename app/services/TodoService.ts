5afc8a85862900dd42af27e5fcf21cd1e433bd13
YK220015 <220015ab@yse-c.net>
2024-02-07 11:58:23 +0900

    a

--- app/services/TodoService.ts
+++ app/services/TodoService.ts
@@ -1,10 +1,11 @@
 import { Todo } from "../models/Todo";
+import TagsInput from "@/app/components/tag/TagsInput";
 
 const API_URL = process.env.NEXT_PUBLIC_API_URL || "";
 
 export const getTodos = async () => {
     //TODO: API URL設定
-    const url = API_URL + "";
+    const url = API_URL + "../api/todo/get";
     try {
         const response = await fetch(url);
         if (response.ok) {
@@ -18,7 +19,7 @@ export const getTodos = async () => {
 export const postTodos = async (todos: Todo[]) => {
     if (!todos) return;
     //TODO: API URL設定
-    const url = API_URL + "";
+    const url = API_URL + "../api/todo/add";
     const data = JSON.stringify(todos);
     //TODO: APIで保存し、データを返す
 }
